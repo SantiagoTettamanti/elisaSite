67d07fe98232c244f65aaedf27f7114757851960
Santiago Tettamanti <santiagotettamanti@Santiagos-Air.attlocal.net>
2020-09-10 18:29:42 -0500

    Added angular routing. Routed all components.

--- project-elisa/src/app/app-routing.module.ts
+++ project-elisa/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AboutMePageComponent} from './about-me-page/about-me-page.component';
+import {ContactPageComponent} from './contact-page/contact-page.component';
+import {CollectionPageComponent} from './collection-page/collection-page.component';
+import {IllustrationsPageComponent} from './illustrations-page/illustrations-page.component';
+import { ProductsPageComponent } from './products-page/products-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'about', component: AboutMePageComponent },
+  { path: 'contact', component: ContactPageComponent },
+  { path: 'collection', component: CollectionPageComponent},
+  { path: 'illustrations', component: IllustrationsPageComponent},
+  { path: 'products', component: ProductsPageComponent},
+  { path: '', component: LandingPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
