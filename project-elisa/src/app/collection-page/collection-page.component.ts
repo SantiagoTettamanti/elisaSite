1dc1f0bc925303d6757e0b4ee9c48011b763de5d
Santiago Tettamanti <santiagotettamanti@Santiagos-Air.attlocal.net>
2020-09-16 16:00:50 -0500

    Updated collection page

--- project-elisa/src/app/collection-page/collection-page.component.ts
+++ project-elisa/src/app/collection-page/collection-page.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionPageComponent implements OnInit {
 
+  // public images = ["../../assets/Coleccion/2 Final.jpg", "../../assets/Coleccion/3 final.jpg" , "../../assets/Coleccion/4 final.jpg", "../../assets/Coleccion/D2.jpg", "jaime5"]
+  
+  public images = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
   constructor() { }
 
   ngOnInit(): void {
+    let a = "a";
+    const b = "b"
   }
 
 }
