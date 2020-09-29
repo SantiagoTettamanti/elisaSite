6910e0272b1c9be5e19b54c819c844a127ffcb8a
SantiagoTettamanti <santiagotettamanti@Santiagos-Air.attlocal.net>
2020-09-29 11:31:08 -0500

    Minor changes

--- src/app/illustrations-page/illustrations-page.component.ts
+++ src/app/illustrations-page/illustrations-page.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./illustrations-page.component.css']
 })
 export class IllustrationsPageComponent implements OnInit {
-
+  
+  public images = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
   constructor() { }
 
   ngOnInit(): void {
