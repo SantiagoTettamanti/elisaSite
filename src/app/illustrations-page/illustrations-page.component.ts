20e6575a56850ed9795feb387272a69baa655bba
SantiagoTettamanti <santiago.tettamanti@gmail.com>
2020-10-06 20:34:33 -0500

    Updated illustrations page. Included all images

--- src/app/illustrations-page/illustrations-page.component.ts
+++ src/app/illustrations-page/illustrations-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import illustrations from '../../assets/Data/Ilustraciones.json'
 @Component({
   selector: 'app-illustrations-page',
   templateUrl: './illustrations-page.component.html',
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IllustrationsPageComponent implements OnInit {
   
-  public images = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+  // public images = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+  public images = illustrations;
   constructor() { }
 
   ngOnInit(): void {
