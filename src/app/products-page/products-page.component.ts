bdb1793710c0658bdbb9bd82505377a7390fb4b2
andresmonc <jaimeandresmonc@gmail.com>
2020-10-06 19:50:34 -0500

    added product images

--- src/app/products-page/products-page.component.ts
+++ src/app/products-page/products-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import products from '../../assets/Data/Product.json';
 @Component({
   selector: 'app-products-page',
   templateUrl: './products-page.component.html',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsPageComponent implements OnInit {
 
-  public images = [1,2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+  public images = products;
   constructor() { }
   
 
